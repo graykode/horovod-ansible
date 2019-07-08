c13b3e905e35f19c1b84d00556728e4bef4dbe22
graykode <nlkey2022@gmail.com>
2019-07-08 13:51:46 +0900

    add volume size option on worker node

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,6 +137,12 @@ resource "aws_instance" "worker" {
   associate_public_ip_address = true # Instances have public, dynamic IP
   source_dest_check = false # TODO Required??
 
+  root_block_device {
+      volume_type = "${var.volume_type}"
+      volume_size = "${var.volume_size}"
+      delete_on_termination = "true"
+  }
+
   availability_zone = "${var.zone}"
   vpc_security_group_ids = ["${aws_security_group.horovod.id}"]
   key_name = "${var.default_keypair_name}"
